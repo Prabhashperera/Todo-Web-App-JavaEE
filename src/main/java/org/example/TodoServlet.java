d624681606773083f4ffe1a1ba4554cd4092f503
Prabhashperera <prabashmperera01@gmail.com>
2025-06-02 21:28:41 +0530

    TDO list Show BE

--- src/main/java/org/example/TodoServlet.java
+++ src/main/java/org/example/TodoServlet.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +11,11 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet("/todo")
 public class TodoServlet extends HttpServlet {
@@ -19,6 +25,19 @@ public class TodoServlet extends HttpServlet {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/todoApp", "root", "1234");
             PreparedStatement stm = connection.prepareStatement("select * from todo");
+            ResultSet rs = stm.executeQuery();
+            List<Map<String, String>> todoList = new ArrayList<>();
+            while (rs.next()) {
+                Map<String, String> todo = new HashMap<>();
+                todo.put("name" , rs.getString("name"));
+                todo.put("description" , rs.getString("description"));
+                todo.put("time", rs.getString("time"));
+                todoList.add(todo);
+            }
+            resp.setContentType("application/json");
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(resp.getWriter(), todoList);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
