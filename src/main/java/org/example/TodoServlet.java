4b251df5090dd7c535a519f10570fdba5154492c
Prabhashperera <prabashmperera01@gmail.com>
2025-06-02 21:12:03 +0530

    DB Config

--- src/main/java/org/example/TodoServlet.java
+++ src/main/java/org/example/TodoServlet.java
@@ -7,11 +7,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 @WebServlet("/todo")
 public class TodoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("<h1>Hello</h1>");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/todoApp", "root", "1234");
+            PreparedStatement stm = connection.prepareStatement("select * from todo");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
