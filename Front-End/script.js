d4afedd7dd21d3910d6910696ff859f4ccf3a1f2
Prabhashperera <prabashmperera01@gmail.com>
2025-06-02 22:01:00 +0530

    TODO LOAD COMPPLETE

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,9 +1,39 @@
+
+
 $(".getReq").click( () => {
     $.ajax({
         url : "http://localhost:8080/app/todo",
         method : "GET",
-        success: ((resp) => {
-            alert("Got the Resp")
-        })
+        success : function (resp) {
+            resp.map((todo) => {
+                console.log(todo)
+                // Create outer div
+                const card = document.createElement('div');
+                card.classList.add('card');
+
+                // Create h3 element
+                const title = document.createElement('h3');
+                title.classList.add('card-title');
+
+                // Create paragraph element
+                const description = document.createElement('p');
+                description.classList.add('card-text');
+
+                // Create time element
+                const time = document.createElement('button');
+                time.classList.add('card-btn' , 'btn', 'btn-primary');
+
+                // Append inner elements to the outer div
+                title.textContent = todo.name;
+                description.textContent = todo.description;
+                time.textContent = todo.time;
+
+                card.appendChild(title);
+                card.appendChild(description);
+                card.appendChild(time);
+
+                $(".todo-holder").append(card);
+            })
+        }
     })
 })
